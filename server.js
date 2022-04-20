c38f730a414b2de73aa02cd02db5f7ed7babe910
Bidesh <bideshbanerjee444@gmail.com>
2022-04-20 22:15:54 +0530

    change respone type

---
+++ server.js
@@ -0,0 +1,35 @@
+const  express =  require('express');
+const  cors = require('cors');
+const fileUplaod = require('express-fileupload');
+const pdfParse = require('pdf-parse'); 
+const app = express();
+
+const PORT = process.env.PORT || 9000;
+app.use(cors());
+
+app.use(fileUplaod());
+app.use(express.json());
+app.get('/',(req,Res) => {
+   Res.send('Hello');
+})
+app.post('/post', async (req,res) => {
+   if(req.files === null) res.send('error occucured');
+      const pdf = req.files.PDF;
+      console.log(pdf);
+      const result = await pdfParse(pdf);
+      const words = result.text.split(" ");
+      console.log(result);
+      res.send(words);
+      
+})
+
+app.listen(PORT,() => {console.log('server is working')})
+/*
+    ***********FORMAT FOR SENDING FILES**************
+   
+   const formData = new FormData();
+   formData.append('json_name', file);
+   const res = await axios.post('http://localhost:9000/post', formData)
+
+
+*/              
